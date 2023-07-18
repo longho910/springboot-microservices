b4fa66d1ad990018505bdfd141e57bd915602415
Long Ho <hoquanglong0910@gmail.com>
2023-07-18 13:11:19 +0700

    create department-service and employee-service

---
+++ department-service/src/test/java/com/longho/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longho.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
