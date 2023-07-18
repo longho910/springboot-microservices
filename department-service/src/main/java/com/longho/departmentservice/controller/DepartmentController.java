26b92a489344393017dcef0c880e48fad01924e1
Long Ho <hoquanglong0910@gmail.com>
2023-07-18 15:01:57 +0700

    add first post method for department-service

---
+++ department-service/src/main/java/com/longho/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,24 @@
+package com.longho.departmentservice.controller;
+
+import com.longho.departmentservice.dto.DepartmentDto;
+import com.longho.departmentservice.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    // Build save department REST API
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto) {
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+
+}
---
+++ department-service/src/main/java/com/longho/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,37 @@
+package com.longho.departmentservice.service.impl;
+
+import com.longho.departmentservice.dto.DepartmentDto;
+import com.longho.departmentservice.entity.Department;
+import com.longho.departmentservice.repository.DepartmentRepository;
+import com.longho.departmentservice.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+        // convert department dto to department jpa entity
+        Department department = new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto.getDepartmentCode()
+        );
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        DepartmentDto savedDepartmentDto = new DepartmentDto(
+                savedDepartment.getId(),
+                savedDepartment.getDepartmentName(),
+                savedDepartment.getDepartmentDescription(),
+                savedDepartment.getDepartmentCode()
+        );
+
+        return savedDepartmentDto;
+    }
+}
